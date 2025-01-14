1a4326b7355cb2d324f75a697f0a05432831c381
r03i22 <103725932+r03i22@users.noreply.github.com>
2025-01-14 16:27:36 +0900

    fix: コメントアウトされたLayout関数を追加

--- app/root.tsx
+++ app/root.tsx
@@ -57,7 +57,7 @@ export const links: LinksFunction = () => [
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
 ];
-
+/*
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
@@ -75,6 +75,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
     </html>
   );
 }
+*/
 
 export default function App() {
   return (
