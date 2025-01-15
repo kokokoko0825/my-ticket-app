cc6be2d1d1474a459215bcdd2be9ff5594a824d4
r03i22 <103725932+r03i22@users.noreply.github.com>
2025-01-15 16:25:24 +0900

    feat: 新しいCloudflare設定とファイル構成を追加

--- app/routes/ticket/$uuid.tsx
+++ app/routes/ticket/$uuid.tsx
@@ -1,5 +1,5 @@
 import { useLoaderData, json } from "@remix-run/react";
-import type { LoaderFunction } from "@remix-run/node";
+import type { LoaderFunction, LoaderFunctionArgs } from "@remix-run/node";
 import { doc, getDoc, updateDoc } from "firebase/firestore";
 import { getFirestore } from "firebase/firestore";
 import {
@@ -9,7 +9,13 @@ import {
 } from "firebase/app";
 
 const firebaseConfig = {
-  // Firebaseの設定
+  apiKey: "AIzaSyCNs9z34dPvegE073RHwmZw3CLYmJ-NsC8",
+  authDomain: "ic-ticket-6fadc.firebaseapp.com",
+  projectId: "ic-ticket-6fadc",
+  storageBucket: "ic-ticket-6fadc.firebasestorage.app",
+  messagingSenderId: "241041666640",
+  appId: "1:241041666640:web:d487ab32df6beda2f01755",
+  measurementId: "G-12ST14XL4P"
 };
 
 let firebaseApp: FirebaseApp;
@@ -21,7 +27,7 @@ if (!getApps().length) {
 
 const db = getFirestore(firebaseApp);
 
-export const loader: LoaderFunction = async ({ params }) => {
+export const loader: LoaderFunction = async ({ params }: LoaderFunctionArgs) => {
   const { uuid } = params;
 
   if (!uuid) {
