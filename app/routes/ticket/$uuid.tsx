3dc3a4e14e9508ab160f55fce1b2c20ffe6f564e
r03i22 <103725932+r03i22@users.noreply.github.com>
2025-01-15 17:42:48 +0900

    fix: UUIDの検証を強化し、エラーメッセージを適切なHTTPステータスコードで返すように修正

--- app/routes/ticket/$uuid.tsx
+++ app/routes/ticket/$uuid.tsx
@@ -1,55 +1,31 @@
 import { useLoaderData, json } from "@remix-run/react";
 import type { LoaderFunctionArgs } from "@remix-run/node";
 import { doc, getDoc, updateDoc } from "firebase/firestore";
-import { getFirestore } from "firebase/firestore";
-import {
-  initializeApp,
-  getApps,
-  FirebaseApp,
-} from "firebase/app";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCNs9z34dPvegE073RHwmZw3CLYmJ-NsC8",
-  authDomain: "ic-ticket-6fadc.firebaseapp.com",
-  projectId: "ic-ticket-6fadc",
-  storageBucket: "ic-ticket-6fadc.firebasestorage.app",
-  messagingSenderId: "241041666640",
-  appId: "1:241041666640:web:d487ab32df6beda2f01755",
-  measurementId: "G-12ST14XL4P"
-};
-
-let firebaseApp: FirebaseApp;
-if (!getApps().length) {
-  firebaseApp = initializeApp(firebaseConfig);
-} else {
-  firebaseApp = getApps()[0];
-}
-
-const db = getFirestore(firebaseApp);
+import { db } from "../../root";
 
 export const loader= async ({ params }: LoaderFunctionArgs) => {
   const { uuid } = params;
 
   if (!uuid) {
-    throw new Error("UUID is required");
+    return json({ status: "error", message: "UUID is required" }, { status: 400 }); // Bad Request
   }
 
   const ticketRef = doc(db, "tickets", uuid);
   const ticketSnap = await getDoc(ticketRef);
 
   if (!ticketSnap.exists()) {
-    return json({ status: "error", message: "Ticket not found" });
+    return json({ status: "error", message: "Ticket not found" },{status: 404}); // Not Found
   }
 
   const ticketData = ticketSnap.data();
 
   if (ticketData.status === "済") {
-    return json({ status: "error", message: "Ticket already used" });
+    return json({ status: "error", message: "Ticket already used" },{status: 409 }); // Conflict
   }
 
   await updateDoc(ticketRef, { status: "済" });
 
-  return json({ status: "success", message: "Ticket updated" });
+  return json({ status: "success", message: "Ticket updated" }, {status: 200}); // OK
 };
 
 export default function Ticket() {
